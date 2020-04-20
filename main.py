310ff6d420c4336a4edcc860fbf0b28c78037849
Frank Piva <piva.post@gmail.com>
2020-04-20 14:14:19 -0700

    add test route, update requirements

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ app = FastAPI()
 @app.get("/")
 def index():
     pass
+
+# Test route for clarity.
+# http://localhost:8000/test/1?q=foo
+@app.get("/test/{test_id}")
+def test(test_id: int, q: str = None):
+    # uncomment these lines to see them on the terminal
+    # print(f"q:<{q}>")
+    # print(f"test_id:<{test_id}>")
+    return {"test_id": test_id, "q": q}
