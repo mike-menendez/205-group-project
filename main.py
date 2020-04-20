3a9e275f67c1ae5297a16c1f5c6dc20f1af0b64b
Mike Menendez <mmenendez@csumb.edu>
2020-04-20 14:59:15 -0700

    Boilerplate (#5)
    * Boilerplate
    
    * add test route, update requirements
    
    * Added two example endpoints
    
    Co-authored-by: Frank Piva <piva.post@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-#!/usr/bin/python3
+from fastapi import FastAPI
+import os, logging, datetime
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    pass
+# Main entrypoint for api. 
+@app.get("/")
+def index():
+    return {"it": "works"}
+
+# Get data by country code
+@app.get("/country/{c_code}")
+def test(c_code: str = None):
+    return {"country": c_code}
