11af05411356d6eca93091ce8097975c87351fc1
Mike <mmenendez@csumb.edu>
2020-04-22 18:42:42 -0700

    injesting data and dynamic rendering it, needs to be massaged first but currently demos it working

--- main.py
+++ main.py
@@ -8,32 +8,35 @@ SLUG_FARM = {}
 
 # Initialize application 
 app = FastAPI()
-
-@app.on_event("startup")
-async def startup_event():
-    async with aiohttp.ClientSession() as session:
-        async with session.get("https://api.covid19api.com/countries") as resp:
-            for x in await resp.json():
-                SLUG_FARM[x["ISO2"]] = x["Slug"]
-    # print(SLUG_FARM, file=sys.stderr)
-
-# Mount static directory as the root 
-app.mount("/static", StaticFiles(directory="static"))
-
 # Mount templates directory for Jinja rendering
 templates = Jinja2Templates(directory="templates")
+
+# Fetches data from the remote api, as the api only supports GET, we're taking a shortcut
+async def data_fetch(loc: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(loc) as resp:
+            return await resp.json()
+
+# Initalize all the slugs needed for getting data from country
+@app.on_event("startup")
+async def startup_event():
+    for x in await data_fetch("https://api.covid19api.com/countries"):
+        SLUG_FARM[x["ISO2"]] = x["Slug"]
+    # Mount static directory as the root 
+    app.mount("/static", StaticFiles(directory="static"))
+
 
 # Index entrypoint for website. 
 @app.get("/")
 async def index(request: Request):
-    async with aiohttp.ClientSession() as session: 
-        async with session.get("https://api.covid19api.com/summary") as resp:
-            return templates.TemplateResponse("index.html", {"request": request, "summary" : await resp.json()})
+    return templates.TemplateResponse("index.html",
+    {"request": request,"summary" : await data_fetch("https://api.covid19api.com/summary")})
 
 # Get data by country code
 @app.get("/country/{c_code}")
 async def test(request: Request, c_code: str = None):
-    if c_code == None:
+    if not c_code or c_code.upper() not in SLUG_FARM.keys():
         return templates.TemplateResponse("404.html", {"request": request})
-    return templates.TemplateResponse("country.html", {"request": request, "data" : SLUG_FARM[c_code.upper()]})
+    return templates.TemplateResponse("country.html",
+        {"request": request, "data" : await data_fetch(f"https://api.covid19api.com/live/country/{c_code}/status/confirmed")})
     
