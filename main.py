1fa299f88d6e97b15229b9b003e7767f901ea938
Mike <mmenendez@csumb.edu>
2020-04-22 20:42:59 -0700

    cleaned up data presentation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import os, logging, datetime, sys, aiohttp
+import os, logging, datetime, sys, aiohttp, numpy, seaborn, sklearn
 
 # Constant slugs for country code id
 SLUG_FARM = {}
@@ -30,13 +30,20 @@ async def startup_event():
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html",
-    {"request": request,"summary" : await data_fetch("https://api.covid19api.com/summary")})
+    {"request": request,"summary" : (await data_fetch("https://api.covid19api.com/summary"))})
 
 # Get data by country code
 @app.get("/country/{c_code}")
 async def test(request: Request, c_code: str = None):
     if not c_code or c_code.upper() not in SLUG_FARM.keys():
         return templates.TemplateResponse("404.html", {"request": request})
+    dead, confirm, recover, active = 0, 0, 0, 0
+    for x in list(await data_fetch(f"https://api.covid19api.com/live/country/{c_code}/status/confirmed")):
+        if "2020-04-23" in x['Date']:
+            dead = dead + x['Deaths']
+            confirm = confirm + x['Confirmed']
+            recover = recover + x['Recovered']
+            active = active + x['Active']
     return templates.TemplateResponse("country.html",
-        {"request": request, "data" : await data_fetch(f"https://api.covid19api.com/live/country/{c_code}/status/confirmed")})
+        {"request": request, "data" : {"dead": dead, "confirmed": confirm, "active": active, "recovered": recover}})
     
