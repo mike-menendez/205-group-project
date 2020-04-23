89bf528808435b25dc52817008766c685b2b5ee5
Mike <mmenendez@csumb.edu>
2020-04-22 17:56:13 -0700

    Created and preloaded Slug farm on startup Created endpoint for country specific data with two letter country code

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import aiohttp
-import os, logging, datetime
+import os, logging, datetime, sys, aiohttp
 
 # Constant slugs for country code id
 SLUG_FARM = {}
 
 # Initialize application 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://api.covid19api.com/countries") as resp:
+            for x in await resp.json():
+                SLUG_FARM[x["ISO2"]] = x["Slug"]
+    # print(SLUG_FARM, file=sys.stderr)
 
 # Mount static directory as the root 
 app.mount("/static", StaticFiles(directory="static"))
@@ -25,6 +32,8 @@ async def index(request: Request):
 
 # Get data by country code
 @app.get("/country/{c_code}")
-def test(request: Request, c_code: str = None):
-    return {"country": c_code}
+async def test(request: Request, c_code: str = None):
+    if c_code == None:
+        return templates.TemplateResponse("404.html", {"request": request})
+    return templates.TemplateResponse("country.html", {"request": request, "data" : SLUG_FARM[c_code.upper()]})
     
