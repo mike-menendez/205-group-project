59bddfbaf3471e12ec31322fce8caecfec0cc7a1
Mike <mmenendez@csumb.edu>
2020-04-29 21:12:17 -0700

    Reformatted all files/pretty printed them per each language's guidelines

--- data_obj.py
+++ data_obj.py
@@ -1,4 +1,9 @@
-import numpy as np, seaborn as sns, sklearn as sk, pandas as pd, matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import sklearn as sk
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 class Data():
     def __init__(self, data):
@@ -11,11 +16,12 @@ class Data():
     # These are compute intensive tasks, would be good to either do this in the background
     # AOT, or we just run them once every 15 mins and the first access takes a hit with
     # subsequent accesses being fast for the next 15 mins with: time.ctime(os.path.getmtime("image.jpg"))
-    
+
     # takes in a dataframe, returns path to vizualization in the format of "static/{c_code}_hist.jpg"
     @staticmethod
     async def hist_viz(d, c_code):
-        if not d.data: return ""
+        if not d.data:
+            return ""
         sns.distplot(d.df['NewDeaths'])
         plt.savefig(f'static/img/{c_code}.svg', dpi=300, format="svg")
         return f'/static/img/{c_code}.svg'
@@ -23,25 +29,29 @@ class Data():
     # takes in a dataframe, returns path to vizualization in the format of "static/{c_code}_viz2.jpg"
     @staticmethod
     async def viz_2(d, c_code):
-        if not d.data: return ""
+        if not d.data:
+            return ""
         pass
 
     # takes in a dataframe, returns path to vizualization in the format of "static/{c_code}_viz3.jpg"
     @staticmethod
     async def viz_3(d, c_code):
-        if not d.data: return ""
+        if not d.data:
+            return ""
         pass
 
     # takes in a dataframe, returns path to vizualization in the format of "static/{c_code}_viz4.jpg"
     @staticmethod
     async def viz_4(d, c_code):
-        if not d.data: return ""
+        if not d.data:
+            return ""
         pass
 
     # takes in a dataframe, returns path to vizualization in the format of "static/{c_code}_reg.jpg"
     @staticmethod
     async def regression(d, c_code):
-        if not d.data: return ""
+        if not d.data:
+            return ""
         pass
 
     # Most likely won't be doing this one as it is extremely compute intensive, I've only gpu trained these
@@ -49,5 +59,6 @@ class Data():
     # takes in a dataframe, returns path to vizualization in the format of "static/{c_code}_arma.jpg"
     @staticmethod
     async def arima(d, c_code):
-        if not d.data: return ""
+        if not d.data:
+            return ""
         pass
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import os, logging, datetime, sys, aiohttp, data_obj
+import os
+import logging
+import datetime
+import sys
+import aiohttp
+import data_obj
 
 # Constant slugs for country code id
 SLUG_FARM = {}
 
-# Initialize application 
+# Initialize application
 app = FastAPI()
 # Mount templates directory for Jinja rendering
 templates = Jinja2Templates(directory="templates")
-# Mount static directory as the root 
+# Mount static directory as the root
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Aux Functions
 # Fetches data from the remote api, as the api only supports GET, we're taking a shortcut
+
+
 async def data_fetch(loc: str):
     async with aiohttp.ClientSession() as session:
         async with session.get(loc) as resp:
@@ -26,11 +33,12 @@ async def startup_event():
     for x in await data_fetch("https://api.covid19api.com/countries"):
         SLUG_FARM[x["ISO2"]] = x["Slug"]
 
-# Index entrypoint for website. 
+# Index entrypoint for website.
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html",
-    {"request": request,"summary" : (await data_fetch("https://api.covid19api.com/summary"))['Global']})
+                                      {"request": request,
+                                       "summary": (await data_fetch("https://api.covid19api.com/summary"))['Global']})
 
 # Admin page route
 @app.get("/admin")
@@ -44,9 +52,12 @@ async def get_all(request: Request, req: str = None):
     if req.lower() not in ["deaths", "recovered", "confirmed"]:
         return templates.TemplateResponse("404.html", {"request": request})
     data = (await data_fetch("https://api.covid19api.com/summary"))['Global']
-    if req.lower() == "deaths": ttl, recent = data['TotalDeaths'], data['NewDeaths']
-    elif req.lower() == "recovered": ttl, recent = data['TotalRecovered'], data['NewRecovered']
-    else: ttl, recent = data['TotalConfirmed'], data['NewConfirmed']
+    if req.lower() == "deaths":
+        ttl, recent = data['TotalDeaths'], data['NewDeaths']
+    elif req.lower() == "recovered":
+        ttl, recent = data['TotalRecovered'], data['NewRecovered']
+    else:
+        ttl, recent = data['TotalConfirmed'], data['NewConfirmed']
     return templates.TemplateResponse("all.html", {"request": request, "title": req.capitalize(), "total": ttl, "new": recent})
 
 # Get data by country code
@@ -56,20 +67,21 @@ async def test(request: Request, c_code: str = None):
         return templates.TemplateResponse("404.html", {"request": request})
     country, data = SLUG_FARM[c_code.upper()], None
     temp = (await data_fetch("https://api.covid19api.com/summary"))['Countries']
-    # data = list(await data_fetch(f"https://api.covid19api.com/live/country/{country}/status/confirmed"))
     for x in temp:
-        if x["Slug"] == country: 
+        if x["Slug"] == country:
             data = x
             break
-    if data is None: return templates.TemplateResponse("404.html", {"request": request})
-    print(f"data: {data}", file=sys.stderr)
-    dead, confirm, recover, active = x['TotalDeaths'], x['TotalConfirmed'], x['TotalRecovered'], (x['TotalConfirmed'] - x['TotalDeaths'] - x['TotalRecovered'])
+    if data is None:
+        return templates.TemplateResponse("404.html", {"request": request})
+    dead, confirm, recover, active = x['TotalDeaths'], x['TotalConfirmed'], x['TotalRecovered'], (
+        x['TotalConfirmed'] - x['TotalDeaths'] - x['TotalRecovered'])
     d = data_obj.Data(temp)
     v1, v2, v3 = await d.hist_viz(d, c_code), await d.viz_2(d, c_code), await d.viz_3(d, c_code),
-    v4, reg, arima = await d.viz_4(d, c_code), await d.regression(d, c_code), await d.arima(d, c_code) 
+    v4, reg, arima = await d.viz_4(d, c_code), await d.regression(d, c_code), await d.arima(d, c_code)
     return templates.TemplateResponse("country.html",
-        {"request": request, "data" : {"dead": dead, "confirmed": confirm, "active": active, "recovered": recover},
-        "country": " ".join(map(lambda x: x.capitalize(), country.split("-"))), "v1": v1, "v2": v2, "v3": v3, "v4": v4, "reg": reg, "ari": arima })
+                                      {"request": request, "data": {"dead": dead, "confirmed": confirm, "active": active, "recovered": recover},
+                                       "country": " ".join(map(lambda x: x.capitalize(), country.split("-"))), "v1": v1, "v2": v2,
+                                       "v3": v3, "v4": v4, "reg": reg, "ari": arima})
 
 # 404 error handling
 @app.get("/.*")
