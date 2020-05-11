84acf4adab4d497530f5ac8c6501702cb394f39d
Mike <mmenendez@csumb.edu>
2020-05-10 21:26:24 -0700

    lowering js line count

--- static/js/a.js
+++ static/js/a.js
@@ -1,6 +1,4 @@
-// mapping of country to country code
 var countries = null
-
 $(document).ready(() => {
     $(".search").on("click", () => {
         if (countries == null) { $.ajax({ url: window.location.href + "preload", async: false, success: (res) => { countries = res } }) }
@@ -9,14 +7,7 @@ $(document).ready(() => {
             window.location.href = "/country/" + countries[country.trim().toLowerCase().replace(/ /g, "-")].toLowerCase() :
             Swal.fire({ title: 'Invalid Country Requested', icon: 'error', text: 'Please Try Again' });
     });
-
-    $(".dcard").on("click", () => {
-        window.location.href = "/all/deaths";
-    });
-    $(".ccard").on("click", () => {
-        window.location.href = "/all/confirmed";
-    });
-    $(".rcard").on("click", () => {
-        window.location.href = "/all/recovered";
-    });
+    $(".dcard").on("click", () => { window.location.href = "/all/deaths"; });
+    $(".ccard").on("click", () => { window.location.href = "/all/confirmed"; });
+    $(".rcard").on("click", () => { window.location.href = "/all/recovered"; });
 });
